406ac77e0898d33d26e4851f57ce946b0bffa0ec
danriz <farukcevik@gmail.com>
2021-08-30 00:00:49 +0300

    Add files via upload

---
+++ eski scriptler/in1.sql
@@ -0,0 +1,3 @@
+insert into yet_kullanicihareket (YET_BASLANGIC_TR, YET_BITIS_TR, YET_KULLANICI_KD, YET_AD, YET_SOYAD, YET_SICIL, YET_BIRIM, YET_SUBE)
+values (to_date('14-02-2009', 'dd-mm-yyyy'), to_date('31-12-2075', 'dd-mm-yyyy'), 'ZERBANK406', 'Z', 'ERBANK406', 'A9756', -1, 406);
+
